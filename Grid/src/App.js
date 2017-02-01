b3a1564c883f6b02146cf38c41b42baae5651069
shunji zhan <shunji.zhan@procore.com>
2017-02-01 14:26:49 -0800

    changed to redux

--- Grid/src/App.js
+++ Grid/src/App.js
@@ -5,60 +5,67 @@ import 'react-virtualized/styles.css'; // only needs to be imported once
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: tableGen(30, 10)
-    };
+    // this.state = {
+    //   data: tableGen(30, 10)
+    // };
     this.cellRenderer = this.cellRenderer.bind(this);
   }
 
   cellRenderer ({ columnIndex, key, rowIndex, style }) {
     return (
       <div key={key} style={style} >
-        {this.state.data[rowIndex][columnIndex]}
+        {this.props.value[rowIndex][columnIndex]}
       </div>
     );  
   }
 
   render() {
-    let list = this.state.data;
+    // let list = this.state.data;
+    let list = this.props.value;
 
     return (
-       <Grid
-        cellRenderer={this.cellRenderer}
-        columnCount={list[0].length}      
-        columnWidth={50}
-        width={list[0].length * 50}
-        // width={500}
-        rowCount={list.length}
-        rowHeight={20}    
-        height={list.length * 20}
-        // height={500}
-      />
+      <div>
+         <Grid
+          cellRenderer={this.cellRenderer}
+          columnCount={list[0].length}      
+          columnWidth={50}
+          width={list[0].length * 50}
+          // width={500}
+          rowCount={list.length}
+          rowHeight={20}    
+          height={list.length * 20}
+          // height={500}
+        />
+        <button onClick={this.props.change}>change</button>
+      </div>
     );
   }
 }
 
 //*** helper functions ***//
-function tableGen(row, colume) {
-  let table = [];
-  let aColume = [];
-  for(let i = 0; i < row; i++) {
-    for(let j = 0; j < colume; j++) {
-      aColume[j] = randomColor();
-    }
-    table[i] = aColume;
-    aColume = [];
-  }
-  return table;
-}
+// function tableGen(row, colume) {
+//   let table = [];
+//   let aColume = [];
+//   for(let i = 0; i < row; i++) {
+//     for(let j = 0; j < colume; j++) {
+//       aColume[j] = randomColor();
+//     }
+//     table[i] = aColume;
+//     aColume = [];
+//   }
+//   return table;
+// }
 
-function randomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 3; i++ ) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// function randomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 3; i++ ) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
 
 export default App;
+
+
+
--- Grid/src/index.js
+++ Grid/src/index.js
@@ -3,7 +3,59 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { createStore } from 'redux'
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case 'CHANGE':
+      let table = tableGen(10,10)
+      return table;
+
+    default:
+      return [['#AAA', '#BBB', '#CCC'], ['#AAA', '#BBB', '#CCC'], ['#AAA', '#BBB', '#CCC']]
+  }
+}
+
+const store = createStore(reducer)
+const rootEl = document.getElementById('root')
+
+const render = () => ReactDOM.render(
+  <App
+    value={store.getState()}
+    change={() => store.dispatch({ type: 'CHANGE' })}
+  />,
+  rootEl
+)
+
+render()
+store.subscribe(render)
+
+
+
+//*** helper functions ***//
+function tableGen(row, colume) {
+  let table = [];
+  let aColume = [];
+  for(let i = 0; i < row; i++) {
+    for(let j = 0; j < colume; j++) {
+      aColume[j] = randomColor();
+    }
+    table[i] = aColume;
+    aColume = [];
+  }
+  return table;
+}
+
+function randomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 3; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );

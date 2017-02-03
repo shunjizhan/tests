2907d53227dfb9c48e53353c56a8d8a0d51d0b9e
shunji zhan <shunji.zhan@procore.com>
2017-02-03 11:56:03 -0800

    forceUpdate works

--- Grid/src/App.js
+++ Grid/src/App.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 import { Grid } from 'react-virtualized';
 import 'react-virtualized/styles.css'; // only needs to be imported once
 
+import { connect } from 'react-redux'
+import { createStore } from 'redux'
+
+// move reducer to a file
+function reducer(state = [], action) {
+	switch (action.type) {
+		case 'randomTable':
+			// return [["QQQ", "WWW"]]
+			return [...action.table]
+
+		case 'clearTable':
+			return []
+
+		default:
+			// return tableGen(3,3)
+			return tableGen(3,3)
+	}
+}
+
+const store = createStore(reducer)
+
+
+const mapState = state => {
+	table: state.table
+}
+
+const mapDispatch = dispatch => {
+	randomTable: () => {
+		dispatch({ 
+			type: 'randomTable', 
+			// table: tableGen(rand(10) * 1.5 + 1, rand(10) + 1),
+			table: tableGen(3,3)
+		})
+	}
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -23,8 +59,9 @@ class App extends Component {
     // let list = this.state.data;
     let list = this.props.table;
 
-    // for(let i = 0; i < list.length; i++) 
-    // 	console.log(list[i])
+    console.log('table:')
+    for(let i = 0; i < list.length; i++) 
+    	console.log(list[i])
 
     if(list[0] != null) {
 	    return (
@@ -43,7 +80,6 @@ class App extends Component {
 
 	        <div id='buttons'>
 	        	<button onClick={this.props.randomTable}>random table</button>
-	        	<button onClick={this.props.clearTable}>clear table</button>
 	        </div>
 	      </div>
 	    );
@@ -59,7 +95,36 @@ class App extends Component {
   }
 }
 
-export default App;
 
+
+//*************** helper functions ***************//
+// reducer file
+function tableGen(row, colume) {
+	let table = [];
+	let aColume = [];
+	for(let i = 0; i < row; i++) {
+		for(let j = 0; j < colume; j++) {
+			aColume[j] = randomColor();
+		}
+		table[i] = aColume;
+		aColume = [];
+	}
+	return table;
+}
+
+function randomColor() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 3; i++ ) {
+		color += letters[Math.floor(Math.random() * 16)];
+		// color += letters[rand(16)];
+	}
+	return color;
+}
+//************************************************//
+
+
+const Table = connect(mapState, mapDispatch)(App)
+export default Table;
 
 
--- Grid/src/index.js
+++ Grid/src/index.js
@@ -1,18 +1,90 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
+import React, { Component } from 'react';
+import { connect, Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { Grid } from 'react-virtualized';
+import 'react-virtualized/styles.css'; // only needs to be imported once
+import './index.css';
+// import Table from './App';
 
-let gen = require('random-seed');
-let date = new Date();
-let rand = gen.create(date.getTime())
+class UpdatingGrid extends Component {
+	render() {
+		return (
+			<Grid 
+				{...this.props}
+			/>
+		)
+	}
+}
 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.cellRenderer = this.cellRenderer.bind(this);
+  }
+
+  cellRenderer ({ columnIndex, key, rowIndex, style }) {
+    return (
+      <div key={key} style={style} >
+        {this.props.table[rowIndex][columnIndex]}
+      </div>
+    );  
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  	if (this.props.table !== prevProps.table) {
+  		this.table.forceUpdate();
+  	}
+  }
+
+
+  render() {
+    let list = this.props.table;
+    console.log(list);
+
+    console.log('table:');
+    for(let i = 0; i < list.length; i++) 
+    	console.log(list[i]);
+
+    if(list[0] != null) {
+	    return (
+	      <div>
+	         <Grid
+	          cellRenderer={this.cellRenderer}
+	          columnCount={list[0].length}      
+	          columnWidth={50}
+	          width={list[0].length * 50}
+	          // width={500}
+	          rowCount={list.length}
+	          rowHeight={20}    
+	          height={list.length * 20}
+	          ref={node => this.table = node}
+	          // height={500}
+	        />
+
+	        <div id='buttons'>
+	        	<button onClick={this.props.randomTable}>random table</button>
+	        	<button onClick={this.props.clearTable}>clear table</button>
+	        </div>
+	      </div>
+	    );
+	} else {
+		return (
+			<div id='buttons'>
+	        	<button onClick={this.props.randomTable}>random table</button>
+	        	<button onClick={this.props.clearTable}>clear table</button>
+	        </div>
+		)
+	}
+
+  }
+}
+
+// move reducer to a file
 function reducer(state = [], action) {
 	switch (action.type) {
 		case 'randomTable':
-			// return [["QQQ", "WWW"]]
 			return action.table
 
 		case 'clearTable':
@@ -22,39 +94,38 @@ function reducer(state = [], action) {
 			return tableGen(3,3)
 	}
 }
+const store = createStore(reducer, []);
 
-const store = createStore(reducer)
-
-const randomTable = () => {
-	store.dispatch({ 
-		type: 'randomTable', 
-		table: tableGen(rand(10) * 1.5 + 1, rand(10) + 1),
-	})
+const mapState = state => {
+	return {
+		table: state
+	}
 }
 
-const clearTable = () => {
-	store.dispatch({ 
-		type: 'clearTable', 
-	})
+const mapDispatch = dispatch => {
+	return {
+		randomTable: () => {
+			dispatch({ 
+				type: 'randomTable', 
+				// table: tableGen(rand(10) * 1.5 + 1, rand(10) + 1),
+				table: tableGen(3,3)
+			})
+		}
+	}
 }
 
-const render = () =>  { 
-	ReactDOM.render(
-		<App
-			table={ store.getState() }
-			randomTable={ randomTable }
-			clearTable={ clearTable }
-		/>,
-		document.getElementById('root')
-	)
-}
+const Table = connect(mapState, mapDispatch)(App);
 
-render()
-store.subscribe(render)
+ReactDOM.render(
+	// wrap this in provider, put store in the react context
+	<Provider store={store}>
+		<Table />
+	</Provider>,
+	document.getElementById('root')
+)
 
-
-
-//*** helper functions ***//
+//*************** helper functions ***************//
+// reducer file
 function tableGen(row, colume) {
 	let table = [];
 	let aColume = [];
@@ -72,9 +143,19 @@ function randomColor() {
 	const letters = '0123456789ABCDEF';
 	let color = '#';
 	for (let i = 0; i < 3; i++ ) {
-		// color += letters[Math.floor(Math.random() * 16)];
-		color += letters[rand(16)];
+		color += letters[Math.floor(Math.random() * 16)];
+		// color += letters[rand(16)];
 	}
 	return color;
 }
+
+
+
+
+
+
+
+
+
+
 

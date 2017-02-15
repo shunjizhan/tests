e153a56ba2df4e861b5099dab1aed032c43912c9
shunji zhan <shunji.zhan@procore.com>
2017-02-15 10:08:37 -0800

    added icons

--- folder-tree/src/App.js
+++ folder-tree/src/App.js
@@ -1,123 +1,77 @@
 import React, { Component } from 'react';
-import { connect, Provider } from 'react-redux'
-import { Grid } from 'react-virtualized';
-import 'react-virtualized/styles.css'; 
+// import { connect, Provider } from 'react-redux'
+
+import FontAwesome from 'react-fontawesome';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.cellRenderer = this.cellRenderer.bind(this);
-  }
-
-  cellRenderer ({ columnIndex, key, rowIndex, style }) {
-    return (
-      <div key={key} style={style} >
-        {this.props.table[rowIndex][columnIndex]}
-      </div>
-    );  
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.props.table)
-    if (this.table != null && this.props.table !== prevProps.table) {
-      this.table.forceUpdate();
-    }
-  }
-
+  // constructor(props) {
+  //   super(props);
+  //   this.cellRenderer = this.cellRenderer.bind(this);
+  // }
 
   render() {
-    let list = this.props.table;
-
-    console.log('table:');
-    for(let i = 0; i < list.length; i++) 
-      console.log(list[i]);
-
-    if(list[0] != null) {
-      return (
-        <div>
-           <Grid
-            cellRenderer={this.cellRenderer}
-            columnCount={list[0].length}      
-            columnWidth={50}
-            width={list[0].length * 50}
-            // width={500}
-            rowCount={list.length}
-            rowHeight={20}    
-            height={list.length * 20}
-            ref={node => this.table = node}
-            // height={500}
-          />
-
-          <div id='buttons'>
-            <button onClick={this.props.randomTable}>random table</button>
-            <button onClick={this.props.clearTable}>clear table</button>
-          </div>
-        </div>
-      );
-  } else {
     return (
-      <div id='buttons'>
-            <button onClick={this.props.randomTable}>random table</button>
-            <button onClick={this.props.clearTable}>clear table</button>
-          </div>
+      <div>
+        <input type="checkbox" id="cbox1" value="first_checkbox" />
+        <FontAwesome name='folder-o'/><br />
+        <FontAwesome name='file-o'/>
+      </div>
     )
-  }
 
   }
 }
 
-const mapState = state => {
-  return {
-    table: state
-  }
-}
+// const mapState = state => {
+//   return {
+//     table: state
+//   }
+// }
 
-const mapDispatch = dispatch => {
-  return {
-    randomTable: () => {
-      dispatch({ 
-        type: 'randomTable', 
-        // table: tableGen(rand(10) * 1.5 + 1, rand(10) + 1),
-        table: tableGen(3,3)
-      })
-    },
-    clearTable: () => {
-      dispatch({
-        type: 'clearTable'
-      })
-    }
-  }
-}
+// const mapDispatch = dispatch => {
+//   return {
+//     randomTable: () => {
+//       dispatch({ 
+//         type: 'randomTable', 
+//         // table: tableGen(rand(10) * 1.5 + 1, rand(10) + 1),
+//         table: tableGen(3,3)
+//       })
+//     },
+//     clearTable: () => {
+//       dispatch({
+//         type: 'clearTable'
+//       })
+//     }
+//   }
+// }
 
-const Table = connect(mapState, mapDispatch)(App);
-export default Table;
+// const Table = connect(mapState, mapDispatch)(App);
+// export default Table;
 
-
-
+export default App;
 
 
 //*************** helper functions ***************//
 // reducer file
-function tableGen(row, colume) {
-  let table = [];
-  let aColume = [];
-  for(let i = 0; i < row; i++) {
-    for(let j = 0; j < colume; j++) {
-      aColume[j] = randomColor();
-    }
-    table[i] = aColume;
-    aColume = [];
-  }
-  return table;
-}
+// function tableGen(row, colume) {
+//   let table = [];
+//   let aColume = [];
+//   for(let i = 0; i < row; i++) {
+//     for(let j = 0; j < colume; j++) {
+//       aColume[j] = randomColor();
+//     }
+//     table[i] = aColume;
+//     aColume = [];
+//   }
+//   return table;
+// }
 
-function randomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 3; i++ ) {
-    color += letters[Math.floor(Math.random() * 16)];
-    // color += letters[rand(16)];
-  }
-  return color;
-}
+// function randomColor() {
+//   const letters = '0123456789ABCDEF';
+//   let color = '#';
+//   for (let i = 0; i < 3; i++ ) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//     // color += letters[rand(16)];
+//   }
+//   return color;
+// }
 //************************************************//
